dd5eeef3bbb8db24d8aa12e7eb0816dc653795a4
jamesmcquarrie <jamesmcquarrie@hotmail.com>
2023-09-27 02:00:36 +0100

    Added a spinner which appears whilst sending an email using the contatc form

---
+++ BeyondMassages.Web/wwwroot/js/submitForm.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    var submitButton = document.getElementById('submitButton');
+    var form = document.querySelector('form');
+
+    submitButton.addEventListener('click', function(e) {
+        e.preventDefault();  // Prevent the form from submitting the traditional way
+        
+        if($(form).valid()) {  // Check if the form is valid
+            submitButton.innerHTML = 'Sending... <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';  // Change button text and add spinner
+            submitButton.setAttribute('disabled', 'disabled');  // Disable the button to prevent multiple submissions
+            form.submit();  // Submit the form using JavaScript
+        }
+    });
+});
